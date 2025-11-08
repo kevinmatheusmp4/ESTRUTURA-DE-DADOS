2842748f87e628a685df6152591cc9c29b76171d
kevinmatheusmp4 <kevinmatheusquaresma@gmail.com>
2025-11-08 18:45:53 -0300

    Add files via upload

---
+++ ESTRUTURA DADOS/TEMA 1 - WAR/NIVEL NOVATO/ESTRUTURAS_LINEARES.c
@@ -0,0 +1,38 @@
+/*
+
+FILAS - Estruturas DE DADOS LINEARES
+
+Uma fila é uma estrutura de dados linear que segue o princípio FIFO (First In, First Out),
+onde o primeiro elemento a ser inserido é o primeiro a ser removido.
+As filas são usadas em diversas aplicações, como gerenciamento de tarefas, impressão de documentos,
+e simulação de processos.
+
+VETORES - Estruturas DE DADOS LINEARES
+
+Uma vetor (ou array) é uma estrutura de dados linear que armazena elementos em posições contíguas de memória.
+Cada elemento pode ser acessado diretamente através de um índice, permitindo acesso rápido e eficiente.
+
+
+
+====================================================
+
+PILHAS - Estruturas DE DADOS LINEARES
+
+Uma pilha é uma estrutura de dados linear que segue o princípio LIFO (Last In, First Out),
+onde o último elemento a ser inserido é o primeiro a ser removido.
+As pilhas são usadas em diversas aplicações, como gerenciamento de chamadas de função,
+desfazer/refazer operações em editores, e avaliação de expressões matemáticas.
+
+
+*/
+
+// EXEMPLO DE ESTRUTURA DE DADOS LINEARES: FILA, PILHA E VETOR
+
+struct ESTRUTURAS_LINEARES //Fila, Pilha, Vetor
+{
+    int dado; //Elemento da estrutura linear (pode ser adaptado conforme a necessidade)
+    struct ESTRUTURAS_LINEARES  *prox;// Ponteiro para o próximo elemento na estrutura linear
+
+    /* data */
+};
+
---
+++ ESTRUTURA DADOS/TEMA 1 - WAR/NIVEL NOVATO/Estruturas_nao_lineares.c
@@ -0,0 +1,58 @@
+/*
+
+ESTRUTURA DE DADOS NÃO LINEARES:
+
+- NÃO LINEARES:
+Organizam os elementos de forma hierárquica (como organogramas ou sistemas de arquivos)
+ou em rede complexas (como mapas, redes sociais ou sistemas de transporte),
+permitindo múltiplas conexões entre os dados.
+
+    - ÁRVORES
+    - GRAFOS   
+
+- REPRESENTAÇÃO DE ESTRUTURAS DE DADOS:
+
+    - LISTA ENCADEADA
+    - MATRIZ DE ADJACÊNCIA
+    - LISTA DE ADJACÊNCIA
+
+====================================================
+
+ARVORES - Estruturas DE DADOS NÃO LINEARES
+
+Uma árvore é uma estrutura de dados não linear que organiza os elementos em uma hierarquia,
+onde cada elemento (nó) pode ter múltiplos filhos, mas apenas um pai.
+
+-----APENAS UM PAI-----
+
+         (RAIZ)
+         /    \
+      (NÓ)   (NÓ)
+      / \       \
+   (NÓ)(NÓ)    (NÓ)
+
+As árvores são usadas em diversas aplicações, como sistemas de arquivos,
+bancos de dados hierárquicos, e algoritmos de busca e ordenação.
+
+====================================================
+
+GRAFOS - Estruturas DE DADOS NÃO LINEARES 
+
+Um grafo é uma estrutura de dados não linear composta por um conjunto de nós (vértices)
+e conexões (arestas) entre esses nós, permitindo múltiplas conexões entre os dados.
+
+    (NÓ)------(NÓ)
+     | \        |
+     |  \       |
+    (NÓ)------(NÓ)
+
+Os grafos são usados em diversas aplicações, como redes sociais, sistemas de transporte,
+e modelagem de relações complexas entre dados.
+
+====================================================
+
+
+
+
+
+*/
