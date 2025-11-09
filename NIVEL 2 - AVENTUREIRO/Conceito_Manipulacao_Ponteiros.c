fb18806ffd688c69e8e2235718bb229df4bf68bd
kevinmatheusmp4 <kevinmatheusquaresma@gmail.com>
2025-11-09 14:03:29 +0000

    Novo arquivo tudo certo nada errado

---
+++ NIVEL 2 - AVENTUREIRO/Conceito_Manipulacao_Ponteiros.c
@@ -0,0 +1,32 @@
+// CONCEITO E MANIUPULAÇÃO DE PONTEIROS
+
+/*
+
+um ponteiro é uma variável especial que armazena o endereço de memória de outra variável.
+No entanto, em vez de guardar um valor, como o número 10 ou a letra A,
+o ponteiro guarda onde esse valor está localizado na memória do computador.
+
+Com isso, ele permite que você acesse, modifique e gerencie dados de maneira muito mais flexível e poderosa.
+
+
+& - Operador "Endereço de": Usado para obter o endereço de memória de uma variável.
+* - Operador "DIREÇAO": Usado para acessar ou modificar o valor armazenado
+
+OPERAÇÕES COM PONTEIROS
+
+1. Declaração de Ponteiros:
+Para declarar um ponteiro, você usa o operador asterisco (*) antes do nome da variável
+
+int *p; // p é um ponteiro para um inteiro
+
+2. Atribuição de Endereço:
+
+int x = 10;
+int *p = &x; // o operador & obtém o endereço de x
+
+3. Acesso ao Valor Apontado:
+
+int valor = *p; // o operador * desreferencia o ponteiro para obter o valor de x
+
+
+*/
